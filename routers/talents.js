cc04d2599e1bb0cdbe28ce7dd603cdbb9ae13691
jurip89 <juri@Barbaras-iMac.local>
2022-10-09 17:59:38 +0200

    added query and route of talents

--- routers/talents.js
+++ routers/talents.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const User = require("../models").user;
-const Image = require('../models').image;
-const Review =require('../models').review
+const Image = require("../models").image;
+const Review = require("../models").review;
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
@@ -9,10 +9,24 @@ router.get("/", async (req, res, next) => {
     const talents = await User.findAll({
       attributes: ["name", "profilePic", "id", "email", "intro"],
       where: { isRecruiter: false },
-        require: false,
-      include:{model:Image, attributes:['source','id']}
+      require: false,
+      include: { model: Image, attributes: ["source", "id"] }
     });
     res.send(talents);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const theOne = await User.findByPk(id,{attributes: ["name", "profilePic", "id", "email", "intro"],
+      where: { isRecruiter: false },
+      require: false,
+      include: [{ model: Image, attributes: ["source", "id"] },{model:Review, attributes: ['comment','id','updatedAt','title'],include:{model:User, attributes:['name','profilePic']}}]});
+    !theOne && res.status(404).send('Missing Person!');
+    res.status(200).send(theOne)
   } catch (error) {
     next(error);
   }
