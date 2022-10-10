c0559e6eaea4e98df2c7341ef4a5c6af30cbed65
jurip89 <juri@Barbaras-iMac.local>
2022-10-10 13:35:18 +0200

    edited query for /talents

--- routers/talents.js
+++ routers/talents.js
@@ -2,15 +2,20 @@ const { Router } = require("express");
 const User = require("../models").user;
 const Image = require("../models").image;
 const Review = require("../models").review;
+const Role = require("../models").role;
+const UserRole = require("../models").userRole;
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
   try {
     const talents = await User.findAll({
-      attributes: ["name", "profilePic", "id", "email", "intro"],
+      attributes: ["name", "profilePic", "id"],
       where: { isRecruiter: false },
-      require: false,
-      include: { model: Image, attributes: ["source", "id"] }
+     include:{
+          model: Role,
+          attributes: ["name", "id"],
+          through: { attributes: [] },
+        },
     });
     res.send(talents);
   } catch (error) {
@@ -21,12 +26,26 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const theOne = await User.findByPk(id,{attributes: ["name", "profilePic", "id", "email", "intro"],
+    const theOne = await User.findByPk(id, {
+      attributes: ["name", "profilePic", "id", "email", "intro"],
       where: { isRecruiter: false },
       require: false,
-      include: [{ model: Image, attributes: ["source", "id"] },{model:Review, attributes: ['comment','id','updatedAt','title'],include:{model:User, attributes:['name','profilePic']}}]});
-    !theOne && res.status(404).send('Missing Person!');
-    res.status(200).send(theOne)
+      include: [
+        { model: Image, attributes: ["source", "id"] },
+        {
+          model: Review,
+          attributes: ["comment", "id", "updatedAt", "title"],
+          include: { model: User, attributes: ["name", "profilePic"] },
+        },
+        {
+          model: Role,
+          attributes: ["name", "id"],
+          through: { attributes: ["yearsOfExperience"] },
+        },
+      ],
+    });
+    !theOne && res.status(404).send("Missing Person!");
+    res.status(200).send(theOne);
   } catch (error) {
     next(error);
   }
